4628033a825367c6e2b9ed2142c27be0eadb986b
EvolOMICS <113347720+EvolOMICS-TU@users.noreply.github.com>
2023-05-17 12:45:05 +0530

    Update ETENLNC_UNIX.sh

--- ETENLNC_UNIX.sh
+++ ETENLNC_UNIX.sh
@@ -629,7 +629,7 @@ Rscript tximport_deseq.R $base $run varianceStabilizingTransformation
 
 #Fixes left shift
 mv -v "$run"\ .csv bak_"$run"\ .csv
-echo -n "Gene_ID", > $run.csv
+echo -n "Transcript_ID", > $run.csv
 cat bak_"$run"\ .csv >> $run.csv
 
 #Filters significant DEGs
@@ -653,7 +653,7 @@ Rscript tximport_deseq.R $base2 $run varianceStabilizingTransformation
 
 #Fixes left shift
 mv -v "$run"\ .csv bak_"$run"\ .csv
-echo -n "Gene_ID", > $run.csv
+echo -n "Transcript_ID", > $run.csv
 cat bak_"$run"\ .csv >> $run.csv
 
 #Filters significant DEGs
@@ -677,7 +677,7 @@ Rscript tximport_deseq.R $base3 $run varianceStabilizingTransformation
 
 #Fixes left shift
 mv -v "$run"\ .csv bak_"$run"\ .csv
-echo -n "Gene_ID", > $run.csv
+echo -n "Transcript_ID", > $run.csv
 cat bak_"$run"\ .csv >> $run.csv
 
 #Filters significant DEGs
